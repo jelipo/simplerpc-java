c4e990dc96140b52ee6643359f162559219791e0
jelipo <me@jelipo.com>
2020-04-30 11:15:09 +0800

    Upgrade dependent version

--- simplerpc/build.gradle.kts
+++ simplerpc/build.gradle.kts
@@ -3,22 +3,22 @@ plugins {
     application
 }
 
-version = "0.0.2"
+version = "0.0.3"
 
 val lombokVersion = "1.18.12"
 
 dependencies {
     implementation("ch.qos.logback:logback-classic:1.2.3")
-    implementation("com.google.guava:guava:28.2-jre")
+    implementation("com.google.guava:guava:29.0-jre")
     implementation("cglib:cglib:3.3.0")
     testCompileOnly("org.projectlombok:lombok:$lombokVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     //用于扫描注解
     implementation("org.reflections:reflections:0.9.12")
     //序列化
-    compileOnly("com.esotericsoftware:kryo:5.0.0-RC4")
+    compileOnly("com.esotericsoftware:kryo:5.0.0-RC5")
     //网络协议
-    compileOnly("io.netty:netty-all:4.1.46.Final")
+    compileOnly("io.netty:netty-all:4.1.49.Final")
     //lombok
     compileOnly("org.projectlombok:lombok:$lombokVersion")
     annotationProcessor("org.projectlombok:lombok:$lombokVersion")
